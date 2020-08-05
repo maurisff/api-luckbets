348d0b742a6742081766d0afa31cb040810c8cd7
mauricio.sff <mauricio.sff@gmail.com>
2020-08-05 14:23:57 -0300

    Implementado notificação de sorteio
    Signed-off-by: mauricio.sff <mauricio.sff@gmail.com>

--- server/services/notificacaoSorteio.js
+++ server/services/notificacaoSorteio.js
@@ -2,40 +2,98 @@
 /* eslint-disable no-use-before-define */
 /* eslint-disable linebreak-style */
 const { CronJob } = require('cron');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const moment = require('moment-timezone');
-// const controller = require('../controllers/sorteio.controller');
+const firebaseHelper = require('../helper/firebase');
+const sorteioRepository = require('../repositories/sorteio.repository');
 
+const ModalidadeModel = mongoose.model('Modalidade');
+const UsuarioModel = mongoose.model('Usuario');
+const ApostaModel = mongoose.model('Aposta');
 const tzLog = process.env.TZSERVER || 'America/Sao_Paulo';
 
-module.exports = {
-  async start() {
-    console.log('inicializando notificação de sorteios...');
-    await schedulerAviso();
-  },
-};
 
-async function schedulerAviso() {
-  // Test expression: https://cronjob.xyz/
-  const cronTime = (process.env.TIME_SCHEDULER_AVISOSORTEIO ? process.env.TIME_SCHEDULER_AVISOSORTEIO : '0 08,18 * * 1-6'); // default 1 vez as 08 e as 18 horas de segunda a sabado
-  // ================================================================================================================================
-  const scheduller = new CronJob(cronTime, (() => {
-    try {
-      notificaUsuarios();
-    } catch (error) {
-      console.error(`${moment().tz(tzLog).format()} - Execution schedulerAviso. Error: `, error);
-    }
-  }), null, true, tzLog);
-  scheduller.start();
-  console.log('Started schedulerAviso!');
-}
-
-async function notificaUsuarios() {
+async function notificaUsuarios(modalidade, concurso) {
   // console.log(`${moment().tz(tzLog).format('DD/MM/YYYY HH:mm:ss')} - Notificando usuarios de sorteios...`);
   try {
-    // await controller.consultaSorteio(modadidade._id, { auto: true });
+    if (!modalidade) {
+      return;
+    }
+    const usuarios = await UsuarioModel.find({ notificaSorteio: { $elemMatch: { $eq: modalidade.codigo } } });
+    if (!usuarios || usuarios.length === 0) {
+      return;
+    }
+    const usuariosFiltrados = usuarios.filter(async (f) => !await ApostaModel.findOne({ usuarioCotaId: f._id, modalidadeId: modalidade._id, proximoConcurso: concurso }));
+    if (!usuariosFiltrados || usuariosFiltrados.length === 0) {
+      return;
+    }
+
+    const notificacoes = usuariosFiltrados.map((u) => ({
+      usuarioId: u._id,
+      notification: {
+        title: 'Hoje tem Sorteio!',
+        body: `Olá ${(u.nome ? u.nome.split(' ')[0] : u.email)}, Hoje tem sorteio do concurso ${concurso} da ${modalidade.titulo}.\nNão se esqueça de fazer sua aposta!`,
+        icon: `${modalidade.codigo.toLowerCase()}.png`,
+        /** Implemenatr action para ignorar proxima notificações. */
+      },
+    }));
+
+    notificacoes.forEach(async (el) => {
+      await firebaseHelper.sendNotificationToUser(el.usuarioId, el.notification);
+    });
   } catch (error) {
     console.error(`${moment().tz(tzLog).format('DD/MM/YYYY HH:mm:ss')} - Notificando usuarios de sorteios. Erro: `, error);
     console.log('===================================================================================================================================');
   }
 }
+
+
+async function verificaSorteioModalidade(modalidade) {
+  // console.log(`${moment().tz(tzLog).format('DD/MM/YYYY HH:mm:ss')} - Verificando sorteios da modalidade ${modalidade.codigo}...`);
+  try {
+    if (!modalidade) {
+      return;
+    }
+    const sorteio = await sorteioRepository.ultimoResultado(modalidade._id);
+    if (!sorteio) {
+      return;
+    }
+
+    if (sorteio.proximaApuracao && moment().tz(tzLog).isSameOrAfter(moment(sorteio.proximaApuracao).tz(tzLog), 'day')) {
+      notificaUsuarios(modalidade, sorteio.proximoConcurso);
+    }
+  } catch (error) {
+    console.error(`${moment().tz(tzLog).format('DD/MM/YYYY HH:mm:ss')} - Verificando sorteios da modalidade ${modalidade.codigo}. Erro: `, error.message);
+    console.log('===================================================================================================================================');
+  }
+}
+
+
+async function schedulerAviso(modalidade) {
+  // await verificaSorteioModalidade(modalidade);
+  // Test expression: https://cronjob.xyz/
+  const cronTime = (process.env.TIME_SCHEDULER_AVISOSORTEIO ? process.env.TIME_SCHEDULER_AVISOSORTEIO : '0 08,18 * * 1-6'); // default 1 vez as 08 e as 18 horas de segunda a sabado
+  // ================================================================================================================================
+  const scheduller = new CronJob(cronTime, (() => {
+    try {
+      verificaSorteioModalidade(modalidade);
+    } catch (error) {
+      console.error(`${moment().tz(tzLog).format()} - Execution schedulerAviso. Error: `, error);
+    }
+  }), null, true, tzLog);
+  scheduller.start();
+  // console.log('Started schedulerAviso!');
+}
+
+
+module.exports = {
+  async start() {
+    console.log('inicializando notificação de sorteios...');
+    const modalidades = await ModalidadeModel.find();
+    if (modalidades && modalidades.length > 0) {
+      await global.util.asyncForEach(modalidades, async (doc) => {
+        await schedulerAviso(doc);
+      });
+    }
+  },
+};
